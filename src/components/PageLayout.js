b4349b59a93d33fd0b6996b9b7c64d2562c4d173
mariana-k <mariana.kyrkosh@gmail.com>
2021-01-14 01:50:20 +0200

    Fixes

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -26,7 +26,7 @@ const PageLayout = props => {
   const classes = useStyles();
 
   React.useEffect(() => {
-    setTimeout(function(){ document.getElementById('loaded').style.display = 'block'; }, 500);
+    setTimeout(function(){ document.getElementById('loaded').style.display = 'block'; }, 100);
   });
 
   return (
